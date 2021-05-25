11cea7c1e1f68420de55cb6c80b549185b636b19
samadii <77415087+samadii@users.noreply.github.com>
2021-05-25 23:21:21 +0430

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-# Infinity Bots
+
 
 import os
 
@@ -6,3 +6,4 @@ class Config(object):
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
     API_ID = int(os.environ.get("API_ID", 12345))
     API_HASH = os.environ.get("API_HASH", "")
+    CAPTION = os.environ.get("CAPTION", "")
