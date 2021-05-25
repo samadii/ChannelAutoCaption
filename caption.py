aa7df385f1f02e7ca092ceca18a32177d779d650
samadii <77415087+samadii@users.noreply.github.com>
2021-05-25 23:16:54 +0430

    Update caption.py

--- caption.py
+++ caption.py
@@ -1,15 +1,10 @@
-# Infinity Bots
+
 
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import FloodWait
 
-@Client.on_message(filters.document & filters.channel)
+@Client.on_message(filters.media & filters.channel)
 async def caption(client, message: Message):
-    await message.edit("Your caption here",
-          reply_markup=InlineKeyboardMarkup(
-            [
-                [InlineKeyboardButton("Button Name", url="https://t.me/JEBotZ")]
-            ]
-                                           )
+    await message.edit("Your caption here"
                       )
