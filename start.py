c394f575fb03bbd74ab5e9ed95f279eb9cbea0b3
samadii <77415087+samadii@users.noreply.github.com>
2021-05-25 23:13:12 +0430

    Update start.py

--- start.py
+++ start.py
@@ -4,4 +4,4 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.command("start") & filters.private)
 async def start(client, message):
-  await message.reply("Hey there, I'm channel caption bot. \n\nSource code => https://github.com/ImJanindu/channel-caption-bot \n\n**@JEBotZ**")
+  await message.reply("Hey there, I'm channel caption bot. \n\nSource code => https://github.com/samadii/ChannelAutoCaption")
