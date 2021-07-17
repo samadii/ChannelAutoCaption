64c5645e53de6664faa776acb422c0ff4d22b211
samadii <77415087+samadii@users.noreply.github.com>
2021-07-17 15:48:18 +0430

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 import os
 
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     API_ID = int(os.environ.get("API_ID", 12345))
     API_HASH = os.environ.get("API_HASH", "")
     CAPTION = os.environ.get("CAPTION", "")
