262d85683c827f927b5f5ff7b511a03fa9136fa9
samadii <77415087+samadii@users.noreply.github.com>
2021-07-20 20:14:24 +0430

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -16,20 +16,22 @@ async def caption(client, message: Message):
     if FILE_NAME and not button_name:
         media = message.audio or message.video or message.document or message.animation
         if (media is not None) and (media.file_name is not None):
-            _file = media.file_name
+            file = media.file_name
+            _file = file.replace("-", " ").replace("_", " ").replace(".mp4", " ").replace(".mkv", " ").replace(".mp3", " ").replace(".zip", " ")
             caption = CAPTION
             await message.edit(
-                f"**{_file}**\n\n"
+                f"{_file}\n\n"
                 f"{caption}")
         else:
             await message.edit(CAPTION)
     if FILE_NAME and button_name: 
         media = message.audio or message.video or message.document or message.animation
         if (media is not None) and (media.file_name is not None):
-            _file = media.file_name
+            file = media.file_name
+            _file = file.replace("-", " ").replace("_", " ").replace(".mp4", " ").replace(".mkv", " ").replace(".mp3", " ").replace(".zip", " ")
             caption = CAPTION
             await message.edit(
-                f"**{_file}**\n\n"
+                f"{_file}\n\n"
                 f"{caption}",
                   reply_markup=InlineKeyboardMarkup(
                 [
