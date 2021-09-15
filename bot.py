af880c7ef9c9a8b4e633a7c624a0c8b780cf46b5
samadii <77415087+samadii@users.noreply.github.com>
2021-09-16 02:13:13 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ class autocaption(Client):
             api_hash = Config.API_HASH,
             workers = 20,
             plugins = dict(
-                root="Plugins"
+                root="plugins"
             )
         )
 
