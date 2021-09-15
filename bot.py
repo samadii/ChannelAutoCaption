bf2c4f7c94448e334a17bc790d81f6a97e175e84
samadii <77415087+samadii@users.noreply.github.com>
2021-09-16 02:05:09 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,29 @@
-# Infinity Bots
+import pyrogram
+
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 import os
-import logging
-import pyrogram
-from config import Config  
 
+from config import Config
+from pyrogram import Client 
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+class autocaption(Client):
+    
+    def __init__(self):
+        super().__init__(
+            session_name="Captioner",
+            bot_token = Config.BOT_TOKEN,
+            api_id = Config.API_ID,
+            api_hash = Config.API_HASH,
+            workers = 20,
+            plugins = dict(
+                root="Plugins"
+            )
+        )
 
 if __name__ == "__main__" :
-    plugins = dict(
-        root="plugins"
-    )
-    JEBotZ = pyrogram.Client(
-        "CaptionBot",
-        bot_token=Config.TG_BOT_TOKEN,
-        api_id=Config.API_ID,
-        api_hash=Config.API_HASH,
-        plugins=plugins,
-        workers=300
-    )
-    JEBotZ.run()
+    autocaption().run()
