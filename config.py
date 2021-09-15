27bcea46c1d2fc973e2a561e384b2339e55d8e0f
samadii <77415087+samadii@users.noreply.github.com>
2021-09-16 01:54:47 +0430

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
-
-
 import os
 
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     API_ID = int(os.environ.get("API_ID", 12345))
     API_HASH = os.environ.get("API_HASH", "")
-    CAPTION = os.environ.get("CAPTION", "")
+    DB_URL = os.environ.get("DATABASE_URL", "")
